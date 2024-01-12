b19c76830c8337797abe065c5d9e5b8c4273ca3b
Valera Leontyev <feedbee@gmail.com>
2024-01-12 22:08:51 +0000

    Adding support of passing config values from the envoronment

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const characterLimit = 4000; // You can change this limit as needed, but not more then 4096
-export const characterLimitLargeText = 10000; // You can change this limit as needed
-export const backendBaseUrl = 'http://localhost:3001'; // Replace with your actual backend URL (ommit the last slash /)
+export const characterLimit = process.env.REACT_APP_CHARACTER_LIMIT || 4000; // Max limit is 4096
+export const characterLimitLargeText = process.env.REACT_APP_CHARACTER_LIMIT_LARGE_TEXT || 10000; // You can change this limit as needed
+export const backendBaseUrl = process.env.REACT_APP_BACKEND_BASE_URL || 'http://localhost:3001'; // Replace with your actual backend URL (omit the last slash /)
