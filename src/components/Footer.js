a37f22af00316979378122cb8b6ac0c2829889fc
Valera Leontyev <feedbee@gmail.com>
2024-02-03 12:08:23 +0000

    Add a note about non-human voice

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,10 @@ const Footer = () => {
   return (
     <footer className="footer mt-4 py-3">
       <div className="container text-center">
-        <p className="small">© {new Date().getFullYear()} AI, Read It! - All rights reserved.</p>
+        <p className="small">
+          © {new Date().getFullYear()} AI, Read It! - All rights reserved. <br />
+          <span className="small text-muted"><em>Note: The voice you hear from the application is AI-generated and not a human voice.</em></span>
+        </p>
       </div>
     </footer>
   );
