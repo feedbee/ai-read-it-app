1a6404ddf04ed6cd4495344fc40369d8210de8fb
Valera Leontyev <feedbee@gmail.com>
2024-02-11 22:07:07 +0000

    Adding a Demo section on main page

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export const characterLimit = process.env.REACT_APP_CHARACTER_LIMIT || 4000; // Max limit is 4096
 export const characterLimitLargeText = process.env.REACT_APP_CHARACTER_LIMIT_LARGE_TEXT || 10000; // You can change this limit as needed
 export const backendBaseUrl = window.DYNAMIC_CONF_BACKEND_BASE_URL || process.env.REACT_APP_BACKEND_BASE_URL || 'http://localhost:3001'; // Replace with your actual backend URL (omit the last slash /)
+export const cdnBaseUrl = process.env.REACT_APP_CDN_BASE_URL || 'http://localhost:3000';
 export const userMode = process.env.REACT_APP_USER_MODE || 'disabled'; // User Auth: disabled, optional, mandatory
 export const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID || '';
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,3 +1,5 @@
+import { cdnBaseUrl } from '../config';
+
 const MainPage = () => (
   <div className="main-page">
     <div className="container">
@@ -21,11 +23,20 @@ const MainPage = () => (
             <p><strong>Versatile Use Cases:</strong> Ideal for audiobooks, e-learning, or enjoying articles audibly.</p>
           </div>
         </div>
-          
+
+        <h2>Demo <i class="bi bi-soundwave"></i></h2>
+        <p>The following voice was recorded using AI, Read It!:</p>
+        <div className="mb-4">
+          <audio controls>
+            <source src={`${cdnBaseUrl}/demo/ai-read-it-demo-1.mp3`} type="audio/mpeg" />
+            Your browser does not support the audio element.
+          </audio>
+        </div>
+
         <div className="alert alert-info d-flex flex-row">
           <i className="fas fa-fw fa-info-circle mr-3 mt-1 "></i>
           <div>
-            To start using the AI, Read It! app, please log in. Currently, we only support Google login,
+            To start using the AI, Read It! app, please log in. Currently, we only support <strong>Google login</strong>,
             offering you a quick and secure way to access all our features. Please, login using the
             button on top right.
           </div>
