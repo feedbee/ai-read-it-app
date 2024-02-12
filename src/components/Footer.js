9ee1ed93f1ac74501f79ae6a2d421279821b51e7
Valera Leontyev <feedbee@gmail.com>
2024-02-12 17:17:33 +0000

    Privacy policy and Terms of service links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import { cdnBaseUrl } from '../config';
+
 const Footer = () => {
   return (
     <footer className="footer mt-4 py-3">
       <div className="container text-center">
-        <p className="small">
-          © {new Date().getFullYear()} AI, Read It! - All rights reserved. <br />
-          <span className="small text-muted"><em>Note: The voice you hear from the application is AI-generated and not a human voice.</em></span>
+        <p className="small gray-links">
+          © {new Date().getFullYear()} AI, Read It! - All rights reserved.<br />
+          <span className="small text-muted"><em>Note: The voice you hear from the application is AI-generated and not a human voice.</em></span><br />
+          <span className="small text-muted"><a href={`${cdnBaseUrl}/legal/ai-read-it-privacy-policy.pdf`} target='_blank' rel='noreferrer'>Privacy Policy</a>. <a href={`${cdnBaseUrl}/legal/ai-read-it-terms-of-service.pdf`} target='_blank' rel='noreferrer'>Terms of Service</a>.</span>
         </p>
       </div>
     </footer>
